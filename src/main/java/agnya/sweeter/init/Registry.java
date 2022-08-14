903ec89f2f2d39222683310a4fad2ff8d523a3ac
nvb-uy <almhak@proton.me>
2022-08-14 14:51:44 -0700

    fix food & cake local key

--- src/main/java/agnya/sweeter/init/Registry.java
+++ src/main/java/agnya/sweeter/init/Registry.java
@@ -26,16 +26,16 @@ public class Registry {
     // ! ITEM REGISTRY
     // Ice Creams
     
-    public static final RegistryObject<Item> VANILLA_ICE_CREAM = ITEMS.register("ice_cream", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab)));
-    public static final RegistryObject<Item> VANILLA_ICE_CREAM_CHERRY = ITEMS.register("ice_cream_with_cherry", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab)));
-    public static final RegistryObject<Item> VANILLA_ICE_CREAM_SPRINKLES = ITEMS.register("ice_cream_with_sprinkles", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab)));
+    public static final RegistryObject<Item> VANILLA_ICE_CREAM = ITEMS.register("ice_cream", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab).food(FoodRegistry.ICE_CREAM)));
+    public static final RegistryObject<Item> VANILLA_ICE_CREAM_CHERRY = ITEMS.register("ice_cream_with_cherry", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab).food(FoodRegistry.ICE_CREAM_WITH_CHERRY)));
+    public static final RegistryObject<Item> VANILLA_ICE_CREAM_SPRINKLES = ITEMS.register("ice_cream_with_sprinkles", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab).food(FoodRegistry.ICE_CREAM_WITH_SPRINKLES)));
 
-    public static final RegistryObject<Item> CHOCOLATE_ICE_CREAM = ITEMS.register("chocolate_ice_cream", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab)));
-    public static final RegistryObject<Item> CHOCOLATE_ICE_CREAM_WITH_CHERRY = ITEMS.register("chocolate_ice_cream_with_cherry", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab)));
-    public static final RegistryObject<Item> CHOCOLATE_ICE_CREAM_WITH_SPINKLES = ITEMS.register("chocolate_ice_cream_with_sprinkles", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab)));
+    public static final RegistryObject<Item> CHOCOLATE_ICE_CREAM = ITEMS.register("chocolate_ice_cream", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab).food(FoodRegistry.CHOCOLATE_ICE_CREAM)));
+    public static final RegistryObject<Item> CHOCOLATE_ICE_CREAM_WITH_CHERRY = ITEMS.register("chocolate_ice_cream_with_cherry", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab).food(FoodRegistry.CHOCOLATE_ICE_CREAM_WITH_CHERRY)));
+    public static final RegistryObject<Item> CHOCOLATE_ICE_CREAM_WITH_SPINKLES = ITEMS.register("chocolate_ice_cream_with_sprinkles", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab).food(FoodRegistry.CHOCOLATE_ICE_CREAM_WITH_SPRINKLES)));
 
-    public static final RegistryObject<Item> STRAWBERRY_ICE_CREAM = ITEMS.register("strawberry_ice_cream", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab)));
-    public static final RegistryObject<Item> STRAWBERRY_ICE_CREAM_WITH_CHERRY = ITEMS.register("strawberry_ice_cream_with_cherry", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab)));
-    public static final RegistryObject<Item> STRAWBERRY_ICE_CREAM_WITH_SPINKLES = ITEMS.register("strawberry_ice_cream_with_sprinkles", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab)));
+    public static final RegistryObject<Item> STRAWBERRY_ICE_CREAM = ITEMS.register("strawberry_ice_cream", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab).food(FoodRegistry.STRAWBERRY_ICE_CREAM)));
+    public static final RegistryObject<Item> STRAWBERRY_ICE_CREAM_WITH_CHERRY = ITEMS.register("strawberry_ice_cream_with_cherry", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab).food(FoodRegistry.STRAWBERRY_ICE_CREAM_WITH_CHERRY)));
+    public static final RegistryObject<Item> STRAWBERRY_ICE_CREAM_WITH_SPINKLES = ITEMS.register("strawberry_ice_cream_with_sprinkles", () -> new Item(new Item.Properties().tab(SweeterTab.SweeterTab).food(FoodRegistry.STRAWBERRY_ICE_CREAM_WITH_SPRINKLES)));
 
 }
