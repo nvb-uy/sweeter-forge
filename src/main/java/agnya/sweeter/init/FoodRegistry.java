084137d0172ad1916dc985ca191d9cb05743977e
nvb-uy <almhak@proton.me>
2022-08-14 15:54:38 -0700

    add alwaysEat() property to ice creams

--- src/main/java/agnya/sweeter/init/FoodRegistry.java
+++ src/main/java/agnya/sweeter/init/FoodRegistry.java
@@ -8,15 +8,15 @@ public class FoodRegistry {
     public static float ICE_CREAM_SATURATION = 0.5f;
     
     
-    public static final FoodProperties ICE_CREAM = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
-    public static final FoodProperties ICE_CREAM_WITH_SPRINKLES = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
-    public static final FoodProperties ICE_CREAM_WITH_CHERRY = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
+    public static final FoodProperties ICE_CREAM = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).alwaysEat().build();
+    public static final FoodProperties ICE_CREAM_WITH_SPRINKLES = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).alwaysEat().build();
+    public static final FoodProperties ICE_CREAM_WITH_CHERRY = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).alwaysEat().build();
 
-    public static final FoodProperties CHOCOLATE_ICE_CREAM = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
-    public static final FoodProperties CHOCOLATE_ICE_CREAM_WITH_CHERRY = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
-    public static final FoodProperties CHOCOLATE_ICE_CREAM_WITH_SPRINKLES = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
+    public static final FoodProperties CHOCOLATE_ICE_CREAM = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).alwaysEat().build();
+    public static final FoodProperties CHOCOLATE_ICE_CREAM_WITH_CHERRY = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).alwaysEat().build();
+    public static final FoodProperties CHOCOLATE_ICE_CREAM_WITH_SPRINKLES = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).alwaysEat().build();
 
-    public static final FoodProperties STRAWBERRY_ICE_CREAM = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
-    public static final FoodProperties STRAWBERRY_ICE_CREAM_WITH_CHERRY = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
-    public static final FoodProperties STRAWBERRY_ICE_CREAM_WITH_SPRINKLES = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
+    public static final FoodProperties STRAWBERRY_ICE_CREAM = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).alwaysEat().build();
+    public static final FoodProperties STRAWBERRY_ICE_CREAM_WITH_CHERRY = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).alwaysEat().build();
+    public static final FoodProperties STRAWBERRY_ICE_CREAM_WITH_SPRINKLES = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).alwaysEat().build();
 }
