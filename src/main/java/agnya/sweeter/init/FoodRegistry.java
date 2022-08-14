064eaa690a58757fcf2febae8cd6b736a56e17e4
nvb-uy <almhak@proton.me>
2022-08-14 11:08:56 -0700

    add food registry

---
+++ src/main/java/agnya/sweeter/init/FoodRegistry.java
@@ -0,0 +1,17 @@
+package agnya.sweeter.init;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class FoodRegistry {
+    // Default Food Values
+    public static int ICE_CREAM_NUTRITION = 2;
+    public static float ICE_CREAM_SATURATION = 0.5f;
+    
+    
+    public static final FoodProperties ICE_CREAM = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
+    public static final FoodProperties CHOCOLATE_ICE_CREAM = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
+    public static final FoodProperties ICE_CREAM_WITH_SPRINKLES = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
+    public static final FoodProperties ICE_CREAM_WITH_CHERRY = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
+    public static final FoodProperties CHOCOLATE_ICE_CREAM_WITH_CHERRY = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
+    public static final FoodProperties CHOCOLATE_ICE_CREAM_WITH_SPRINKLES = new FoodProperties.Builder().nutrition(ICE_CREAM_NUTRITION).saturationMod(ICE_CREAM_SATURATION).build();
+}
